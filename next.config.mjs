c96e57741de1a98c053b0c9b566ee9047ef499fb
IgorOCosta <igorcosta@live.com>
2024-09-25 06:37:41 -0300

    build

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "export",
-    trailingSlash: true, // Garante que URLs funcionem como diretórios em páginas estáticas
-    // outros ajustes que você possa precisar...
+    // output: "export",
+    // trailingSlash: true, // Garante que URLs funcionem como diretórios em páginas estáticas
+    // // outros ajustes que você possa precisar...
 };
 
 export default nextConfig;
