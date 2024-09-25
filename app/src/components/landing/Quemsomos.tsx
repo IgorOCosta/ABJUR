6dfb4c7d6b9ef407686c8ce8507c4c8f4fbc6958
IgorOCosta <igorcosta@live.com>
2024-09-25 06:45:15 -0300

    build

--- app/src/components/landing/Quemsomos.tsx
+++ app/src/components/landing/Quemsomos.tsx
@@ -1,7 +1,7 @@
 'use client';
 import React from 'react';
 import MissionVisionValues from './MissionVisionValues';
-import styles from './QuemSomos.module.css';
+import styles from './Quemsomos.module.css';
 import OurStory from './Nossahistoria';
 import VideoSection from './VideoSection';
 
