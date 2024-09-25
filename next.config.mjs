10249deae23595ed08b50f4f6226f175aafa0b9f
IgorOCosta <igorcosta@live.com>
2024-09-25 05:52:18 -0300

    first deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: "export",
+    trailingSlash: true, // Garante que URLs funcionem como diretórios em páginas estáticas
+    // outros ajustes que você possa precisar...
+};
 
 export default nextConfig;
